1407fa1a631bcde8bae4f75c0387dbd0abff75c3
mysticanaturae <mysticaholistica888@gmail.com>
2026-07-12 09:01:51 -0600

    Add email API

---
+++ api/send-email.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+
+    res.status(200).json({
+
+        success: true,
+
+        message: "Blinkita Multiverse Email API is alive ✨"
+
+    });
+
+}
